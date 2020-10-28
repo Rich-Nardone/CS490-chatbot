328f23757b647da8c6a4a0d1d6d3aaddf137701d
EC2 Default User <ec2-user@ip-172-31-24-154.ec2.internal>
2020-10-28 02:13:06 +0000

    Add unit test files and lint

--- models.py
+++ models.py
@@ -8,38 +8,37 @@ class Messages(db.Model):
     message = db.Column(db.String(120))
     who = db.Column(db.String(120))
     time = db.Column(db.String(120))
-    
+
     def __init__(self, a, b, c):
         self.message = a
         self.who = b
         self.time = c
-        
+
     def __repr__(self):
         return self.message, self.who, self.time
+
 
 class Responses(db.Model):
     message = db.Column(db.String(120), primary_key=True)
     response = db.Column(db.String(120))
-    
+
     def __init__(self, a, b):
         self.response = a
         self.message = b
-        
+
     def __repr__(self):
         return self.response
-        
+
+
 class Authuser(db.Model):
     name = db.Column(db.String(120), primary_key=True)
     propic = db.Column(db.String(120))
     sid = db.Column(db.String(255))
-    
+
     def __init__(self, a, b, c):
         self.name = a
         self.propic = b
         self.sid = c
-        
+
     def __repr__(self):
         return self.name, self.propic, self.sid
-
-
-
